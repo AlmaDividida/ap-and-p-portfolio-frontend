1c550340562b73116f6532cbd27bb13b1912f4f0
davidmoreno <david_626180497@hotmail.com>
2024-08-20 07:22:38 -0600

    fixes sobre nosotros page

--- src/app/pages/inicio/sobre-nosotros/sobre-nosotros.component.ts
+++ src/app/pages/inicio/sobre-nosotros/sobre-nosotros.component.ts
@@ -2,24 +2,32 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { DarkModeService } from '../../../services/dark-mode.service';
 import { RouterModule } from '@angular/router';
+import { AnimationOptions, LottieComponent } from 'ngx-lottie';
+import path from 'path';
 
 @Component({
   selector: 'app-sobre-nosotros',
   standalone: true,
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    LottieComponent,
   ],
   templateUrl: './sobre-nosotros.component.html',
   styleUrl: './sobre-nosotros.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SobreNosotrosComponent {
+  options: AnimationOptions;
   constructor(
     protected darkModeService: DarkModeService,
-  ) {}
+  ) {
+    this.options = {
+      path: 'https://lottie.host/2fdd211e-8e4a-40a7-b260-87353994798b/EgwU6kNLCc.json',
+    };
+  }
 
-  getColorClass() {
-    return this.darkModeService.getDarkMode() ? 'color-dark' : 'color-light';
+  getDarkMode() {
+    return this.darkModeService.getDarkMode();
   }
 }
